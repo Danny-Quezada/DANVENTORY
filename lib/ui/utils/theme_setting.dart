f3609fbe2d2af91fcd8e011e6f7ed6cc03b9116c
Danny-Quezada <danny.quezada32u@std.uni.edu.ni>
2025-01-11 10:04:51 -0600

    Adding ThemeSetting and Colors' configuration

---
+++ lib/ui/utils/theme_setting.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class ThemeSetting {
+  static const Color principalColor = Color(0xff4D96FF);
+  static const Color redColor = Color(0xffFF6B6B);
+  static const Color greenColor = Color(0xff6BCB77);
+  static const Color borderColor = Color(0xFFC2BFBF);
+
+  static ThemeData themeData = ThemeData(
+      useMaterial3: true,
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: const WidgetStatePropertyAll(principalColor),
+          textStyle: WidgetStateProperty.all(
+            const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      fontFamily: "Poppins",
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: const WidgetStatePropertyAll(Colors.white),
+          backgroundColor: WidgetStateProperty.all(principalColor),
+          textStyle: WidgetStateProperty.all(
+            const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          fixedSize: const WidgetStatePropertyAll(Size(double.maxFinite, 50)),
+          shape: WidgetStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+          ),
+        ),
+      ),
+      colorScheme: ColorScheme.fromSeed(seedColor: principalColor),
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16.0),
+          borderSide: const BorderSide(color: borderColor),
+        ),
+      ));
+}
